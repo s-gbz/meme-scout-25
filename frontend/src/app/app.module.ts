c456a7ed05ee15f5aa4925ad4be8356cdfcd2247
Sergej Grilborzer <sergej@grilborzer.de>
2020-05-23 17:50:25 +0200

    FE: Add Firebase

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,19 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpErrorInterceptor } from './service/http-error.interceptor';
 import { AuthorizationInterceptor } from './service/authorization.interceptor';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5nmeWmR8rhTKjok1KEP6Xi742LAq3JeA",
+  authDomain: "meme-scout-25.firebaseapp.com",
+  databaseURL: "https://meme-scout-25.firebaseio.com",
+  projectId: "meme-scout-25",
+  storageBucket: "meme-scout-25.appspot.com",
+  messagingSenderId: "130563424299",
+  appId: "1:130563424299:web:9cef89b1c942d0297caa6e"
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +33,10 @@ import { AuthorizationInterceptor } from './service/authorization.interceptor';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [
     StatusBar,
