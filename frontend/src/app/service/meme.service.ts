df5bf840b0167201345b395789985b135824c940
Sergej Grilborzer <sergej@grilborzer.de>
2020-05-25 11:48:12 +0200

    Adapt MemeService to handling ID arrays

--- frontend/src/app/service/meme.service.ts
+++ frontend/src/app/service/meme.service.ts
@@ -14,15 +14,18 @@ export class MemeService{
     constructor(private afStore:AngularFireStorage ) { }
     
     public async getMemes(): Promise<Meme []> {
-        const exampleMemeRef = "gs://meme-scout-25.appspot.com/memes/tumblr_m2j9xbYh8q1r9pr63o1_500.jpg";
-        const memeStoragePath = this.storageRef.refFromURL(exampleMemeRef);
+        // TODO: Substitute with memeIDs when available
+        const exampleMemeRef = ["tumblr_m2j9xbYh8q1r9pr63o1_500.jpg", "tumblr_m4hrgjTOiS1r9pr63o1_500.jpg", "tumblr_m4tnu5kx2s1qd5giho1_500.jpg", "tumblr_m5yxn0ms7z1r9pr63o1_500.jpg", "tumblr_m893njtVJ81r9pr63o1_500.jpg"]
         const newMemes : Meme [] = [];
 
-        await memeStoragePath.getDownloadURL().then(
-            (url) => {                
-                newMemes.push(this.createNewMeme("abc", url, ["tag1", "tag2", "tag3"]));
-            }
-        )
+
+        exampleMemeRef.forEach(memeRef => {
+            const memeStoragePath = this.storageRef.refFromURL("gs://meme-scout-25.appspot.com/memes/" + memeRef);
+            this.requestNewMeme(memeStoragePath).then(
+                (newMeme) => newMemes.push(newMeme)
+            );
+        });
+        
 
         return newMemes;
     }
@@ -37,6 +40,18 @@ export class MemeService{
         
         this.afStore.upload(path, file);
 
+    }
+
+    private async requestNewMeme(memeStoragePath: firebase.storage.Reference) {
+        let newMeme: Meme;
+
+        await memeStoragePath.getDownloadURL().then(
+            (newUrl) => {                
+                newMeme = this.createNewMeme("abc", newUrl, ["tag1", "tag2", "tag3"])
+            }
+        )
+
+        return newMeme;
     }
 
     private createNewMeme(id: string, fileUrl: string, tags? :string []): Meme {
