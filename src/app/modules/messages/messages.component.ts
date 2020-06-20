ba403f6d340b13defd2c695a9dc6ed33dfd0b51c
Aleksej Popovic <aleksej.popovic146@hotmail.de>
2020-06-20 18:06:44 +0200

    FE: Implement Message View

--- src/app/modules/messages/messages.component.ts
+++ src/app/modules/messages/messages.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['messages.component.scss']
 })
 export class Messages {
-
   constructor() {}
 
+  messages = [
+    {
+      user: 'aleksej',
+      createdAt: 1554090856000,
+      msg: 'Beep beep test 123'
+    },
+    {
+      user: 'katharzyna',
+      createdAt: 1554090956000,
+      msg: ':)'
+    },
+    {
+      user: 'sergej',
+      createdAt: 1554091056000,
+      msg: 'Ich liebe Programmierschnittstellen!!!!! :DDD'
+    }
+  ];
+
+  currentUser = 'aleksej';
+  newMsg = '';
+  @ViewChild(IonContent, {static:false}) content: IonContent
+
+  sendMessage(){
+    this.messages.push({
+      user: 'aleksej',
+      createdAt: new Date().getTime(),
+      msg: this.newMsg
+    });
+
+    this.newMsg = '';
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    });
+  }
 }
